0e377340ad8ff2cd0a4d31347fb07b1e5339861f
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-04-05 10:25:11 +1200

    profit goal finished

--- 06_profit_goal.py
+++ 06_profit_goal.py
@@ -1,51 +1,61 @@
-
-
-def profit_goal(total_costs):
+def get_goal(total_costs):
+    error = "Please enter a dollar amount or percentage."
     profit_type = 0
     valid = False
     while not valid:
+        # get response
+        profit_goal = input("Profit goal? ")
+
+        # check for $ or % sign, split string into profit type and amount
+        if profit_goal[0] == "$":
+            profit_type = "$"
+            profit = int(profit_goal[1:])
+
+        elif profit_goal[-1] == "%":
+            profit_type = "%"
+            profit = int(profit_goal[:-1])
+
+        else:
+            profit_type = "unknown"
+            profit = profit_goal
+
+        # check that number is valid
         try:
-            profit_goal = input("Profit goal? ")
+            profit_goal = float(profit_goal)
+            if profit_goal <= 0:
+                print(error)
+                continue
 
-            # check for $ or % sign, split string into profit type and amount
-            if profit_goal[0] == "$":
-                profit_type = "$"
-                profit = int(profit_goal[1:])
-
-            elif profit_goal[-1] == "%":
-                profit_type = "%"
-                profit = int(profit_goal[:-1])
-
-            else:
-                
-                profit_goal = int(profit_goal)
-                # if only given a number, ask what they meant based on number
-                if profit_goal <= 100:
-                    profit = input("Did you mean {}%".format(profit_goal))
-                    if profit_goal == "yes":
-                        profit_type = "%"
-
-                elif profit_goal > 100:
-                    profit = input("Did you mean ${}".format(profit_goal))
-                    profit_goal = "$"
-                else:
-                    print("Please enter a dollar amount or percentage.")
-                    continue
-        
         except ValueError:
-            print("Please enter a dollar amount or percentage.")
+            print(error)
+            continue
+
+        # if only given a number, ask what they meant based on number
+        if profit_goal <= 100 and profit_type == "unknown":
+            profit = input("Did you mean {}%? ".format(profit_goal))
+            if profit == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        elif profit_goal > 100 and profit_type == "unknown":
+            profit = input("Did you mean ${}? ".format(profit_goal))
+            if profit == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
 
         # calculate target based on profit type
         if profit_type == "%":
-            profit /= 100
-            target = total_costs * profit
+            target = total_costs * (profit_goal / 100)
+            return target
         else:
-            target = profit
-        return target
+            return profit_goal
 
 total_costs = 200
 
-goal = profit_goal(total_costs)
-# print results
-print("Profit Target: ${:.2f}".format(goal))
-print("Total Sales: ${:.2f}".format(goal + total_costs))
+for item in range(6):
+    goal = get_goal(total_costs)
+    # print results
+    print("Profit Target: ${:.2f}".format(goal))
+    print("Total Sales: ${:.2f}".format(goal + total_costs))
